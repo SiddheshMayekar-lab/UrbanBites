bc04ad74edca9bd54b46718c47eaab87f73f0e3f
SiddheshMayekar-lab <siddhesh08mayekar@gmail.com>
2026-03-04 22:05:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY > 0 ){
+        navbar.classList.add("scroll-nav");
+    }
+    else{
+        navbar.classList.remove("scroll-nav");
+    }
+
+=======
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY > 0 ){
+        navbar.classList.add("scroll-nav");
+    }
+    else{
+        navbar.classList.remove("scroll-nav");
+    }
+
+>>>>>>> f0e1b58e01869dca620b9b41aa15566f97779ce2
+})
